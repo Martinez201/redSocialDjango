7e435a67fc9a6eab301595ebeec2fff97fb9b64a
Martinez201 <jesus.martinez.288@estudiante.iesoretania.es>
2022-08-01 10:29:38 +0200

    registrado las tablas al sitio de administración de Django

--- red_social/social/admin.py
+++ red_social/social/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post,Profile
 
 # Register your models here.
+
+admin.site.register(Profile)  # registra la tabla para que se pueda ver desde el sitio de administración de django
+admin.site.register(Post)
+
