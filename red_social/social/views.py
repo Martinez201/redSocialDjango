8d485ede8c4c81aef930f017c19aafe2a6c90912
Martinez201 <jesus.martinez.288@estudiante.iesoretania.es>
2022-08-01 10:59:29 +0200

    añadido listado de posts

--- red_social/social/views.py
+++ red_social/social/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+from .models import *
+
 
 def feed(request):
-    return render(request, 'social/feed.html')
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts
+    }
+    return render(request, 'social/feed.html', context)
+
 
 def profile(request):
     return render(request, 'social/profile.html')
